82ae9081c763088fee0ec8c44d4f706ed4a15217
Kumrnm <84617660+kumrnm@users.noreply.github.com>
2022-09-25 00:48:54 +0900

    v1.00

---
+++ README.md
@@ -0,0 +1,27 @@
+# Tab Auto Tag
+
+[Tablacus Explorer](https://tablacus.github.io/explorer.html) のアドオンです。
+
+## 機能
+
+設定画面から **タグ名** (Tag Names) を登録すると、名前にタグ名を含むフォルダ及びその子孫フォルダを開いた際に、タブにタグ名が表示されるようになります。
+
+（例）`C:\Users\foo-bar\baz\qux` を開いた際の標準のタブ名は `qux` ですが、`bar` をタグ名として登録すると、タブ名は `qux (bar)` となります。
+
+## 導入方法
+
+[tabautotag.zip](https://github.com/kumrnm/aviutl-authentic-grouping/releases/download/v1.00/tabautotag.zip) を解凍し、Tablacus Explorer の addons フォルダに配置します。
+
+## 設定項目
+
+アドオン一覧のオプションボタン（歯車ボタン）から変更できます。
+
+- Tag Names : タグ名
+    - カンマまたは改行で区切ることで複数登録できます。
+
+- Exclude : 除外文字列
+    - フォルダ名のうち、タグ名の検索の対象としない文字列を指定します。
+    - （例）`aria` をタグ名として登録した場合、`MariaCurie` フォルダはこのタグにヒットしますが、この挙動が望ましくない場合、例えば `Maria` を除外文字列に指定することで回避できます。
+
+- Hide tag if folder name contains same text
+    - フォルダ名がタグ名を含む場合はタグ名を非表示にします。
---
+++ script.js
@@ -0,0 +1,51 @@
+const Addon_Id = "tabautotag";
+
+const config = GetAddonElement(Addon_Id);
+if (!config.getAttribute("Set")) {
+	config.setAttribute("HideDuplication", "1");
+}
+
+if (window.Addon == 1) {
+
+	function getConfig(key) {
+		return config.getAttribute(key);
+	}
+
+	function escapeForRegExp(text) {
+		return text.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
+	}
+
+	function regExpFromTextList(textList, regExpOption) {
+		if (!textList) return null;
+
+		const texts = textList.split(/[,\n] */).filter(Boolean);
+		if (texts.length == 0) return null;
+
+		return new RegExp(texts.map(escapeForRegExp).join("|"), regExpOption);
+	}
+
+	const reTags = regExpFromTextList(getConfig("TagNames"));
+	const reExclude = regExpFromTextList(getConfig("Exclude"), "g");
+	const hideDuplication = Boolean(getConfig("HideDuplication"));
+
+	if (reTags) {
+		AddEvent("GetTabName", function (Ctrl) {
+			let path = api.GetDisplayNameOf(Ctrl, SHGDN_FORPARSING | SHGDN_FORPARSINGEX);
+			let tabName = path.substr(path.lastIndexOf("\\") + 1);
+
+			if (reExclude) path = path.replace(reExclude, ",,,,,,,,");
+			const match = path.match(reTags);
+			if (match) {
+				const tagName = match[0];
+				if (!hideDuplication || tabName.indexOf(tagName) < 0) {
+					tabName += " (" + tagName + ")";
+				}
+			}
+
+			return tabName;
+		}, true);
+	}
+
+} else {
+	SetTabContents(0, "", await ReadTextFile("addons\\" + Addon_Id + "\\options.html"));
+}
